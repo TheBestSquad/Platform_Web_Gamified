df1a0a7171a73dcc865f451946092c57391365d9
LuisFer <luisfer1@gmail.com>
2026-04-04 16:25:15 -0300

    feat: adicionar a lógica de login e uma página inicial de placeholder

--- gamification/views.py
+++ gamification/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
+# Bloquear o acesso de quem não estiver logado.
+@login_required 
+def home_view(request):
+    return render(request, 'home.html')
