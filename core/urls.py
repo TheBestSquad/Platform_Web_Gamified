9dbf1bdbd378e53912e1c9305b432cc7a0cec2c9
leobrda <leonardo-breda1@hotmail.com>
2026-04-08 15:14:22 -0300

    feat: setup courses app and Licao model with media support

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from accounts import views as accounts_views
 from django.contrib.auth import views as auth_views
 
@@ -10,3 +12,6 @@ urlpatterns = [
     # Adiciona as rotas padrão do Django (login, logout, password_reset)
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
