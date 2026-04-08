e0af7ec42f97dd44e5f4dc1daef9ccc75b9c15d9
leobrda <leonardo-breda1@hotmail.com>
2026-04-08 14:17:07 -0300

    feat: implement manual student approval for professors

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register/professor/', views.register_professor, name='register_professor'),
     path('register/aluno/', views.register_aluno, name='register_aluno'),
+    path('aprovar-aluno/<int:aluno_id>/', views.aprovar_aluno, name='aprovar_aluno'),
 ]
