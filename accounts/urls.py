5c55be7ee08459027726c304196014bdcf86daf5
leobrda <leonardo-breda1@hotmail.com>
2026-04-13 15:34:23 -0300

    feat: student management dashboard for professors with grade average

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/professor/', views.register_professor, name='register_professor'),
     path('register/aluno/', views.register_aluno, name='register_aluno'),
     path('aprovar-aluno/<int:aluno_id>/', views.aprovar_aluno, name='aprovar_aluno'),
+    path('meus-alunos/', views.lista_alunos_professor, name='lista_alunos'),
 ]
